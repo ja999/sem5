eeb040d7c59d62d48069d8caa0154a395a2b6afd
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2013-11-04 04:06:55 +0100

    eh

--- kck/lab4/main.py
+++ kck/lab4/main.py
@@ -3,32 +3,40 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-import csv
-import glob
-from scipy import randn
-from matplotlib import rc
 from matplotlib import colors
 import colorsys
-from skimage import filter, measure, data, io, img_as_float
-from skimage.morphology import disk
+from skimage import filter, measure, data, io, img_as_float, morphology
+from skimage.morphology import disk, square
 from skimage.filter.rank import autolevel
 
+def invert(v):
+  return 1.0 - v
+
+def process(v):
+  th = np.amin(v) + ((np.amax(v) - np.amin(v)) / 3.1)
+  binary = (v < th)
+  binary = morphology.closing(binary, square(2))
+  binary = morphology.closing(binary, square(16))
+  return binary
+
 def main():
-  original = data.imread('samolot03.jpg')
-  ima = data.imread('samolot04.jpg', as_grey = True)
-  im = ima**6
-  thresh = filter.threshold_otsu(ima)
-  binary = (ima > thresh)
-  io.imshow(binary)
-  contours = measure.find_contours(binary, 0.1)
-  print len(contours)
-  # colorki = ['green', 'red', 'blue', 'black', 'yellow', 'magenta', 'white', 'cyan', 'brown', 'silver']
-  for n, contour in enumerate(contours):
-    plt.plot(contour[:, 1], contour[:, 0], linewidth=2)
-  plt.axis('image')
-  plt.xticks([])
-  plt.yticks([])
-  plt.show()
+  for i in range(0, 13):
+    fig = plt.figure()
+    if i < 10:
+      fname = 'samolot0' + str(i) + '.jpg'
+    else:
+      fname = 'samolot' + str(i) + '.jpg'
+    original = data.imread(fname)
+    im = data.imread(fname, as_grey = True)
+    binary = process(im)
+    io.imshow(original)
+    contours = measure.find_contours(binary, 0.9)
+    for n, contour in enumerate(contours):
+      plt.plot(contour[:, 1], contour[:, 0], linewidth=2)
+    plt.axis('image')
+    plt.xticks([])
+    plt.yticks([])
+    plt.savefig(str(i) + '_5.jpg', bbox_inches = 0, frameon = False)
 
 
 if __name__ == "__main__":
