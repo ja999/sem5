7d28bf0e522daa7d25c776d5713e30ecdab6f373
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2013-11-14 06:24:51 +0100

    [KCK][Michael] lab4 - na 5 revised

--- kck/lab4/a.py
+++
@@ -1,7 +0,0 @@
-from skimage import data
-from skimage.morphology import disk
-from skimage.filter.rank import autolevel, enhance_contrast
-# Load test image
-ima = data.camera()
-# Stretch image contrast locally
-auto = enhance_contrast(ima, disk(20))
