cc2eefee039d3659c1d06ceb59c3587ac5b1b3a7
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-01-07 22:49:01 +0100

    frequency matching the scale, plotting half of the fourier tranformation, etc...

--- kck/sound/wave.py
+++ kck/sound/wave.py
@@ -7,18 +7,21 @@ from scipy import *
 import scipy.io.wavfile
 
 print 'reading file...'
-w, array2 = scipy.io.wavfile.read('err.wav')
+w, array2 = scipy.io.wavfile.read('1khz.wav')
 print 'read!'
 
 print 'sampling frequency:'
 print w
 
 print 'splitting channels...'
-array = np.array([s[0] for s in array2])
+if array2[0].size == 1:
+  array = array2
+else:
+  array = np.array([s[0] for s in array2])
 print 'splitted!'
 
-cut = 0.1
-start = 0
+cut = 0.5
+start = 0.4
 size = array.size
 
 T = size / w
@@ -27,12 +30,13 @@ print size
 print 'time of the file:'
 print T
 
-T = cut * T
+Tend = cut * T
+Tstart = start * T
 print 'time taken into account from file start:'
 print T
 
-n = T * w        # liczba próbek
-t = linspace(0, T, n, endpoint=False) # punkty na osi OX [s]
+n = (Tend - Tstart) * w        # liczba próbek
+t = linspace(Tstart, Tend, n, endpoint=False) # punkty na osi OX [s]
 
 # f = lambda t : sin(100*2*pi*t)    # def. funkcji
 # print f(t)
@@ -61,11 +65,18 @@ signal1 = fft(signal)
 signal1 = abs(signal1)        # moduł
 print 'calculated fft!'
 
+print 'cutting fft...'
+signal_cut = signal1[:(signal1.size / 2)]
+max_amp_freq = np.argmax(signal_cut) / (Tend - Tstart)
+
+print 'frequency with maximum amplitude:'
+print max_amp_freq
+
 subplot(212)
 signal1[0] = 0
-a = linspace(0, w, n, False, False)
+a = linspace(0, w / 2, n / 2, False, False)
 print 'plotting...'
-stem(a, signal1, '-*')
+stem(a, signal_cut, '-*')
 print 'plotted! :)'
 
 show()
